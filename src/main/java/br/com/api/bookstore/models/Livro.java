c056a38cd8056398facdd2fc8dfc62997fceb6f3
Everton Ulisses de Mendonca <evertonulisystem@gmail.com>
2022-02-05 13:37:32 -0300

    Criação do Pacote Models com Livro e Categoria

---
+++ src/main/java/br/com/api/bookstore/models/Livro.java
@@ -0,0 +1,88 @@
+package br.com.api.bookstore.models;
+
+import java.util.Objects;
+
+public class Livro {
+	
+	private Integer id;
+	private String titulo;
+	private String nome;
+	private String texto;
+	
+	private Categoria categoria;
+
+	public Livro() {
+		
+	}
+
+	public Livro(Integer id, String titulo, String nome, String texto, Categoria categoria) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.nome = nome;
+		this.texto = texto;
+		this.categoria = categoria;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
+}
