7fd7ce0a97a70c33b75be1a9d40260004576f8f5
Everton Ulisses de Mendonca <evertonulisystem@gmail.com>
2022-02-05 16:07:50 -0300

    Definindo novo perfis DEV e habilitando MySQL

---
+++ src/main/java/br/com/api/bookstore/config/DevConfig.java
@@ -0,0 +1,27 @@
+package br.com.api.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import br.com.api.bookstore.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciaBaseDeDados() {
+		if(strategy.equals("create")) {
+			this.dbService.instanciaBaseDeDados();
+		}
+		return false;
+	}
+}
