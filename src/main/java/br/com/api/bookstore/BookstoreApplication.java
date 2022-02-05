165de44e7b56627324e0934bf1ba27b2dc2d1283
Everton Ulisses de Mendonca <evertonulisystem@gmail.com>
2022-02-05 15:15:06 -0300

    Instancia automatica com H2

--- src/main/java/br/com/api/bookstore/BookstoreApplication.java
+++ src/main/java/br/com/api/bookstore/BookstoreApplication.java
@@ -1,13 +1,42 @@
 package br.com.api.bookstore;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.api.bookstore.models.Categoria;
+import br.com.api.bookstore.models.Livro;
+import br.com.api.bookstore.repositories.CategoriaRepository;
+import br.com.api.bookstore.repositories.LivroRepository;
 
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner{
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private LivroRepository livroRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica","Livros de TI");
+		
+		Livro l1 = new Livro(null, "Java Man","Everton","Aprendendo Java", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+		
+	}
+	
+	
 
 }
