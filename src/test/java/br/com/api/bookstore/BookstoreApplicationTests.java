0b5bb407791e2cfb6d6f9b5b1afe80ef5cb44804
Everton Ulisses de Mendonca <evertonulisystem@gmail.com>
2022-02-06 16:36:33 -0300

    Introdução a testes

--- src/test/java/br/com/api/bookstore/BookstoreApplicationTests.java
+++ src/test/java/br/com/api/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,37 @@
 package br.com.api.bookstore;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BookstoreApplicationTests {
+import br.com.api.bookstore.controllers.CategoriaController;
+import br.com.api.bookstore.models.Categoria;
+import br.com.api.bookstore.services.CategoriaService;
 
+@SpringBootTest
+class BookstoreApplicationTests{
+
+	@Autowired
+	private CategoriaController categoriaController;
+	
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	
 	@Test
 	void contextLoads() {
 	}
 
+	
+	@Test
+	public void testCadastroCategoria() {
+		Categoria categoria = new Categoria();
+		categoria.setNome("Livro Teste");
+		categoria = categoriaController.create(categoria).getBody();
+		
+		assertEquals(true, categoria.getId() >0);
+	}
+	
 }
